940046ebb7dd29be12210296c9623f5d677ca30a
NISdev1 <85438176+NISdev1@users.noreply.github.com>
2021-08-30 15:20:49 +0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,45 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- #region -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#0000FF" />
+  <meta name="description" content="Deploy containerized applications easily | Ixata" />
+  <meta property="og:url" content="https://www.ixata.tk">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Deploy containerized applications easily | Ixata">
+  <meta property="og:description" content="Deploy containerized applications easily | Ixata">
+  <meta property="og:site_name" content="Ixata">
+  <meta property="og:image" content="logo192">
+  <meta property="og:image:alt" content="Ixata">
+  <!-- #endregion -->
+
+  <!-- #region -->
+  <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+  <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+  <link rel="icon" href="%PUBLIC_URL%/logo512.png" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+  <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+  <!-- #endregion -->
+
+  <title>Ixata | CaaS Based Platform For Developers</title>
+</head>
+
+<body>
+  <noscript>You need to enable JavaScript to run this app.</noscript>
+  <div id="root"></div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.3/feather.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+  <script src="./script.js"></script>
+</body>
+
+</html>
