65c8dbf6e9503fbb40897a5eb9d3bbf1eb8eb6fc
NISdev1 <85438176+NISdev1@users.noreply.github.com>
2021-08-30 15:16:33 +0600

    Create Pricing.js

---
+++ src/components/Pricing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Pricing() {
+    return (
+        <section className="py-7 bg-dark section-angle top-right bottom-right" id="pricing">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mx-auto text-center">
+                        <h2 className="text-white heading-black">Container Pricing.</h2>
+                        <p className="text-light lead">Try our Free Plan with Ads</p>
+                    </div>
+                </div>
+            </div></section>
+    )
+}
+
+export default Pricing
